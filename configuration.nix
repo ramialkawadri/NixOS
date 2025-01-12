ffbf088d3f728e54468f4fe5b31e193b8dd6fe89
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-01-12 09:15:06 +0100

    Add wayland

--- configuration.nix
+++ configuration.nix
@@ -42,6 +42,7 @@
   # Packages
   nixpkgs.config.allowUnfree = true;
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
   services.desktopManager.plasma6.enable = true;
   environment.systemPackages = with pkgs; [
     lshw
