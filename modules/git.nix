190acd791183a1bb5a9155fbb95dd31daf96be25
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-05-03 12:56:52 +0200

    Better git settings

--- modules/git.nix
+++ modules/git.nix
@@ -6,12 +6,18 @@
       user.name  = "Rami Alkawadri";
       user.email = "ramialkawadri@hotmail.com";
       credential.helper = "store";
+
+      interactive = {
+        diffFilter = "delta --color-only";
+      };
+
       delta = {
         line-numbers = true;
         paging = "never";
         dark = true;
         syntax-theme = "Visual Studio Dark+";
         side-by-side = true;
+        hyprlinks = true;
       };
     };
   };
