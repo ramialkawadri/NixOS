cd4d0da16f8871889c7a1513a694d9aa6662518f
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-05-20 19:32:47 +0200

    Always lock before sleep

--- modules/hypridle.nix
+++ modules/hypridle.nix
@@ -7,6 +7,7 @@
           after_sleep_cmd = "hyprctl dispatch dpms on";
           ignore_dbus_inhibit = false;
           lock_cmd = "hyprlock";
+          before_sleep_cmd = "hyprlock";
         };
 
         listener = [
