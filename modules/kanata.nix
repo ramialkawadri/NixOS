2c743e9096090e165f653bf63b685059885fdf56
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-06-03 18:10:57 +0200

    Remove home row mods

--- modules/kanata.nix
+++ modules/kanata.nix
@@ -6,32 +6,14 @@
       config = ''
         (defsrc
           caps
-          a s d f h j k l ;
         )
 
         (deflayer base
           @caps
-          @a  @s  @d  @f  @h  @j  @k  @l  @;
-        )
-
-        (defvar
-          tap-time  155
-          hold-time 155
         )
 
         (defalias
           caps (tap-dance 200 (esc caps))
-          a (tap-hold $tap-time $hold-time a lalt)
-          s (tap-hold $tap-time $hold-time s lsft)
-          d (tap-hold $tap-time $hold-time d lctl)
-          f (tap-hold $tap-time $hold-time f lmet)
-
-          j (tap-hold $tap-time $hold-time j rmet)
-          k (tap-hold $tap-time $hold-time k rctl)
-          l (tap-hold $tap-time $hold-time l rsft)
-          ; (tap-hold $tap-time $hold-time ; ralt)
-
-          h (tap-dance $tap-time (h enter))
         )
       '';
 
