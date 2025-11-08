a970b4d188eb3878090f1fc35932910fb052a914
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-11-08 22:36:08 +0100

    Install nix-shell plugin for zsh

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -5,6 +5,20 @@
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     history.size = 10000;
+    plugins = [
+      # Used to start nix-shell in zsh
+      {
+        name = "zsh-nix-shell";
+        file = "nix-shell.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "chisui";
+          repo = "zsh-nix-shell";
+          rev = "v0.8.0";
+          sha256 = "1lzrn0n4fxfcgg65v0qhnj7wnybybqzs4adz7xsrkgmcsr0ii8b7";
+        };
+      }
+    ];
+
     shellAliases = {
       update = "sudo nixos-rebuild switch";
       upgrade = "sudo nix flake update && sudo nixos-rebuild switch --upgrade";
